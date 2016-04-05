ed2ea5c946ed75ab027dad6b789b9b912196b1c9
lx1993333 <lx1993333@sina.com>
2016-04-05 09:50:26 -0700

    recover

---
+++ 2D_UFO/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D rd2d;
+	public float Speed;
+
+	void Start()
+	{
+		rd2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movment = new Vector2 (moveHorizontal, moveVertical);
+		rd2d.AddForce(movment * Speed);
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag ("PickUp")) 
+		{
+			other.gameObject.SetActive (false);
+		}
+	}
+
+
+}
---
+++ roll a ball/Assets/Scripts/PlayController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayController : MonoBehaviour 
+{
+
+	public float speed;
+	public Text countText;
+	public Text WinText;
+
+	private Rigidbody rb;
+	private int count;
+
+	// Use this for initialization
+
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		WinText.text = "";
+	}
+	
+	// Update is called once per frame
+
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical); 
+
+		rb.AddForce (movement * speed);
+	
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick_up")) 
+		{
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			WinText.text = "You Win!!";
+		}
+	}
+}
+
